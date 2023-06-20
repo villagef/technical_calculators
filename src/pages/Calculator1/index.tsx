f1793717506ff5cbe419e02d01a171b563c5c62a
villagef <filwyd123@gmail.com>
2023-06-20 16:11:21 +0200

    add react-hook-form and first calculator

--- src/pages/Calculator1/index.tsx
+++ src/pages/Calculator1/index.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
+import "./style.css";
+import { useState } from "react";
+
+interface FormValues {
+  f: string;
+  fn: number;
+  r: number;
+}
 
 const Calculator1 = () => {
-  return <div>Calculator 1</div>;
+  const { register, handleSubmit } = useForm<FormValues>();
+  const [result, setResult] = useState<number | null>(null);
+
+  const onSubmit = (data: FormValues) => {
+    const { f, fn, r } = data;
+    const x = (Number(f) * fn) / r;
+    setResult(Number(x));
+  };
+
+  return (
+    <div className="form-wrapper">
+      <h1>Kalkulator</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="form-container">
+        <FormControl>
+          <InputLabel id="dropdown-label">f - wsp. tarcia tocznego</InputLabel>
+          <Select
+            labelId="dropdown-label"
+            id="f"
+            {...register("f", { required: true })}
+          >
+            <MenuItem value={0.5}>O,5 – koło: stal // droga: stal</MenuItem>
+            <MenuItem value={0.15}>O,15 – koło: guma // droga: beton</MenuItem>
+            <MenuItem value={4.5}>4,5 – koło: guma // droga: asfalt</MenuItem>
+          </Select>
+        </FormControl>
+
+        <TextField
+          type="number"
+          label="Fn - siła nacisku[N]"
+          {...register("fn", { required: true })}
+        />
+
+        <TextField
+          type="number"
+          label="r - promień koła[mm]"
+          {...register("r", { required: true })}
+        />
+
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
+      {result && <h1>Ft = {result}[N]</h1>}
+    </div>
+  );
 };
 
 export default Calculator1;
